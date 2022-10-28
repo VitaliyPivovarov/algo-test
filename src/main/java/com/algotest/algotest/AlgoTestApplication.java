dc1abce9b9dc4e9298668dccbfd39136fd30a4b0
vitalypivovarov <v.pivovarov@innopolis.ru>
2022-10-28 18:11:42 +0300

    init

---
+++ src/main/java/com/algotest/algotest/AlgoTestApplication.java
@@ -0,0 +1,13 @@
+package com.algotest.algotest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AlgoTestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AlgoTestApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/algotest/algotest/AlgoTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.algotest.algotest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AlgoTestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
